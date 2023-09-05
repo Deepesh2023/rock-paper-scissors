72318c7863916b82fda08473ca8c172ccb199cbc
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-09-05 15:15:58 +0530

    added button functionality and cleaned up the code

--- script.js
+++ script.js
@@ -32,10 +32,6 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-// get users input `
-// let getUserInput;
-// prompt("Choose your Worrior \n Rock \n Paper \n Scissors");
-// getUserInput = getUserInput.toLowerCase();
 let userChoice;
 let computerChoice = getComputerChoice();
 
@@ -51,17 +47,3 @@ paper.addEventListener("click", () => {
   userChoice = 2;
   winner(userChoice, computerChoice);
 });
-
-// compare it to the array and get the `number-
-// if (getUserInput == worriors[0]) {
-//   userChoice = 0;
-// } else if (getUserInput == worriors[1]) {
-//   userChoice = 1;
-// } else if (getUserInput == worriors[2]) {
-//   userChoice = 2;
-// }
-
-// console.log ("you chose " + worriors[userChoice] + "\n" + "computer chose " + worriors[computerChoice]);
-
-// compare the number to the computers `choice
-// declare winner if any or declare tie`
