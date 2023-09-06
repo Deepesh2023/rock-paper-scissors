4ed73e6e71e7838771725cfeea1bd336fa916dd3
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-09-06 15:11:57 +0530

    removed some js and added icons and display properties

--- script.js
+++ script.js
@@ -8,24 +8,34 @@ function getComputerChoice() {
 }
 
 // the winnning logic
-function winner(playerSelection, computerSelection) {
-  result = function (winner, loser) {
-    result = worriors[winner] + " beats " + worriors[loser];
-    return result;
-  };
+// function winner(playerSelection, computerSelection) {
+//   result = function (winner, loser) {
+//     result = worriors[winner] + " beats " + worriors[loser];
+//     return result;
+//   };
 
-  if (playerSelection == computerSelection) {
-    alert("Its a tie");
-  } else if (computerSelection == playerSelection + 1) {
-    alert("you won! " + result(playerSelection, computerSelection));
-  } else if (playerSelection == computerSelection + 1) {
-    alert("You lose! " + result(computerChoice, playerSelection));
-  } else if (playerSelection == 2 && computerSelection != 1) {
-    alert("you won! " + result(playerSelection, computerSelection));
-  } else if (computerSelection == 2 && playerSelection != 1) {
-    alert("You lose! " + result(computerSelection, playerSelection));
-  }
+//   if (playerSelection == computerSelection) {
+//     alert("Its a tie");
+//   } else if (computerSelection == playerSelection + 1) {
+//     alert("you won! " + result(playerSelection, computerSelection));
+//   } else if (playerSelection == computerSelection + 1) {
+//     alert("You lose! " + result(computerChoice, playerSelection));
+//   } else if (playerSelection == 2 && computerSelection != 1) {
+//     alert("you won! " + result(playerSelection, computerSelection));
+//   } else if (computerSelection == 2 && playerSelection != 1) {
+//     alert("You lose! " + result(computerSelection, playerSelection));
+//   }
+// }
+
+// displaying the choices
+function choiceDisplay(playerSelection, computerSelection) {
+  player.textContent = playerSelection;
+  computer.textContent = computerSelection;
 }
+
+const display = document.querySelector(".display");
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
 
 // selecting the worrior
 const rock = document.querySelector(".rock");
@@ -37,13 +47,16 @@ let computerChoice = getComputerChoice();
 
 rock.addEventListener("click", () => {
   userChoice = 0;
-  winner(userChoice, computerChoice);
+  choiceDisplay(worriors[userChoice], worriors[computerChoice]);
+  // winner(userChoice, computerChoice);
 });
 scissors.addEventListener("click", () => {
   userChoice = 1;
-  winner(userChoice, computerChoice);
+  choiceDisplay(worriors[userChoice], worriors[computerChoice]);
+  // winner(userChoice, computerChoice);
 });
 paper.addEventListener("click", () => {
   userChoice = 2;
-  winner(userChoice, computerChoice);
+  choiceDisplay(worriors[userChoice], worriors[computerChoice]);
+  // winner(userChoice, computerChoice);
 });
